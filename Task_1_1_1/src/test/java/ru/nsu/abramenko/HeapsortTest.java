70df9fa5411dfe131c82ff20d16ffacbdc6c3603
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-09-04 23:21:49 +0700

    upd3

--- Task_1_1_1/src/test/java/ru/nsu/abramenko/HeapsortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/abramenko/HeapsortTest.java
@@ -6,11 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HeapsortTest {
     @Test
-    void SampleTest(){
+    void sampleTest(){
         assertArrayEquals(new int[]{1,2,3,4,5},Main.heapsort(new int[]{5,4,3,2,1}));
     }
     @Test
-    void RandomTest(){
+    void checkMain(){
+        Main.main(null);
+        assertTrue(true);
+    }
+    @Test
+    void randomTest(){
         int arr1[] = new int[]{1,4,22,1564,166,854,0};
         int arr2[]= new int[]{0,1,4,22,166,854,1564};
         assertArrayEquals(arr2,Main.heapsort(arr1));

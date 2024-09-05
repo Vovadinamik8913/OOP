34cd71eb0176161714a82a7f41172e924fb0a9b9
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-09-05 13:07:22 +0700

    nt4

--- Task_1_1_1/src/main/java/ru/nsu/abramenko/Heapsort.java
+++ Task_1_1_1/src/main/java/ru/nsu/abramenko/Heapsort.java
@@ -1,7 +1,7 @@
 package ru.nsu.abramenko;
 
 public class Heapsort {
-    /**
+    /** Преобразование кучи относительно некоторого элемента
      *
      * @param arr
      * @param n
@@ -11,10 +11,10 @@ public class Heapsort {
         int largest = i;
         int l = 2 * i + 1;
         int r = 2 * i + 2;
-        if (l < n && arr[l] > arr[largest]){
+        if (l < n && arr[l] > arr[largest]) {
             largest = l;
         }
-        if (r < n && arr[r] > arr[largest]){
+        if (r < n && arr[r] > arr[largest]) {
             largest = r;
         }
         if (largest != i) {
@@ -25,17 +25,17 @@ public class Heapsort {
         }
     }
 
-    /**
+    /** Сортировка массива, чрезе создание кучи и перестраивание через heapify
      *
      * @param arr
      * @return
      */
     public static int[] heapsort(int[] arr) {
         int n = arr.length;
-        for (int i = n / 2 - 1; i >= 0; i--){
+        for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
         }
-        for (int i = n-1; i >= 0; i--){
+        for (int i = n-1; i >= 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
--- Task_1_1_1/src/test/java/ru/nsu/abramenko/HeapsortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/abramenko/HeapsortTest.java
@@ -9,26 +9,26 @@ import org.junit.jupiter.api.Test;
 
 class HeapsortTest {
     @Test
-    void sampleTest(){
+    void sampleTest() {
         assertArrayEquals(new int[] {1, 2, 3, 4, 5},Heapsort.heapsort(new int[] {5, 4, 3, 2, 1}));
     }
     @Test
-    void checkMain(){
+    void checkMain() {
         Heapsort.main(null);
         assertTrue(true);
     }
     @Test
-    void randomTest(){
+    void randomTest() {
         int[] arr1 = new int[] {1, 4, 22, 1564, 166, 854, 0};
         int[] arr2 = new int[] {0, 1, 4, 22, 166, 854, 1564};
         assertArrayEquals(arr2,Heapsort.heapsort(arr1));
     }
 
     @Test
-    void bigRandomTesr(){
+    void bigRandomTesr() {
         int[] arr = new int[10000];
         Random random = new Random();
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt();
         }
         Heapsort.heapsort(arr);

d14fd3c0d1fe66bc9e269d9ad42f001c53c79a72
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-09-05 09:54:01 +0700

    upd7

--- Task_1_1_1/src/main/java/Heapsort.java
+++ Task_1_1_1/src/main/java/Heapsort.java
@@ -3,10 +3,12 @@ public class Heapsort {
         int largest = i;
         int l = 2*i + 1;
         int r = 2*i + 2;
-        if (l < n && arr[l] > arr[largest])
+        if (l < n && arr[l] > arr[largest]){
             largest = l;
-        if (r < n && arr[r] > arr[largest])
+        }
+        if (r < n && arr[r] > arr[largest]){
             largest = r;
+        }
         if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
@@ -17,8 +19,9 @@ public class Heapsort {
 
     public static int[] heapsort(int arr[]) {
         int n = arr.length;
-        for (int i = n / 2 - 1; i >= 0; i--)
+        for (int i = n / 2 - 1; i >= 0; i--){
             heapify(arr, n, i);
+        }
         for (int i=n-1; i>=0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
--- Task_1_1_1/work_please.sh
+++ Task_1_1_1/work_please.sh
@@ -1,5 +1,5 @@
 javac ../Task_1_1_1/src/main/java/Heapsort.java
 javadoc ../Task_1_1_1/src/main/java/Heapsort.java
-
+java -cp ../Task_1_1_1/src/main/java Heapsort
 
 echo "IT IS WORKING"

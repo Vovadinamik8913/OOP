ac39ad3eb65f85a9341b8823908a4693b0fb7aec
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-09-05 09:40:55 +0700

    upd5

--- Task_1_1_1/src/main/java/Heapsort.java
+++ Task_1_1_1/src/main/java/Heapsort.java
@@ -1,16 +1,12 @@
 public class Heapsort {
-    public static void heapify(int arr[], int n, int i)
-    {
+    public static void heapify(int arr[], int n, int i) {
         int largest = i;
         int l = 2*i + 1;
         int r = 2*i + 2;
-
         if (l < n && arr[l] > arr[largest])
             largest = l;
-
         if (r < n && arr[r] > arr[largest])
             largest = r;
-
         if (largest != i)
         {
             int swap = arr[i];
@@ -20,8 +16,7 @@ public class Heapsort {
         }
     }
 
-    public static int[] heapsort(int arr[])
-    {
+    public static int[] heapsort(int arr[]) {
         int n = arr.length;
         for (int i = n / 2 - 1; i >= 0; i--)
             heapify(arr, n, i);

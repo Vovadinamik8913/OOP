fcd0f6b8d4e1c96c3a31bd4537ce71c3328d7049
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-09-10 15:16:43 +0700

    11-upd 4.0

--- Task_1_1_1/work_please.sh
+++
@@ -1,5 +0,0 @@
-javadoc -d build/docs/javadoc -sourcepath src/main/java -subpackages ru.nsu.abramenko
-javac src/main/java/ru/nsu/abramenko/Heapsort.java -d ./build
-java -cp ./build ru.nsu.abramenko.Heapsort
-
-echo "IT IS WORKING"

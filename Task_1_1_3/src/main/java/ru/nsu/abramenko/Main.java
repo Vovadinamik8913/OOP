39c9a47176151d8eb80ea66248fab1f7a3d2728b
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-09-14 19:55:54 +0700

    add test

--- Task_1_1_3/src/main/java/ru/nsu/abramenko/Main.java
+++ Task_1_1_3/src/main/java/ru/nsu/abramenko/Main.java
@@ -8,6 +8,8 @@ import ru.nsu.abramenko.math.Expression;
 import ru.nsu.abramenko.math.Sub;
 import ru.nsu.abramenko.math.Variable;
 
+import java.util.Scanner;
+
 /** main.
  *
  */
@@ -18,20 +20,11 @@ public class Main {
      * @param args string
      */
     public static void main(String[] args) {
-        Expression e = new Add(new Number(3), new Mul(new Number(2),
-                new Variable("x")));
+        Scanner scanner = new Scanner(System.in);
+        String expression = scanner.nextLine();
+        Expression e = Parser.parseExpr(expression, new int[] {0});
         e.print();
-
-        Expression de = e.derivative("x");
-        de.print();
-
-        double result = e.eval("x = 10; y = 13");
-        System.out.println(result);
-
-        e = new Sub(new Add(new Number(2),
-                new Variable("x")), new Add(new Variable("x"), new Number(2)));
-        de = e.simplify();
-        e.print();
+        Expression de = e.simplify();
         de.print();
     }
 }
--- Task_1_1_3/src/test/java/ru/nsu/abramenko/ExpressionTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/abramenko/ExpressionTest.java
@@ -1,13 +1,54 @@
 package ru.nsu.abramenko;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.nsu.abramenko.math.*;
+import ru.nsu.abramenko.math.Number;
 
 class ExpressionTest {
     @Test
     void mainCheck() {
         Main.main(null);
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("AddTest")
+    void addTest() {
+        Expression e = new Add(new Number(3), new Mul(new Number(2),
+                new Variable("x")));
+        e.print();
+
+        Expression de = e.derivative("x");
+        de.print();
+
+        assertEquals(23.0, e.eval("x = 10; y = 13"));
+    }
+
+    @Test
+    @DisplayName("MulTest")
+    void mulTest() {
+        Expression e = new Mul(new Number(3), new Div(new Number(2),
+                new Variable("x")));
+        e.print();
+
+        Expression de = e.derivative("x");
+        de.print();
+
+        assertEquals(12.0, e.eval("x = 0.5"));
+    }
+
+    @Test
+    @DisplayName("SubEquals")
+    void subEqualsTest() {
+        Expression e = new Sub(new Add(new Number(2),
+                new Variable("x")), new Add(new Variable("x"), new Number(2)));
+        Expression de = e.simplify();
+        e.print();
+        de.print();
         assertTrue(true);
     }
 }

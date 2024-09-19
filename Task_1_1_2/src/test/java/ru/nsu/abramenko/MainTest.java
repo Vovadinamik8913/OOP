f9e7bc633b4b911f3f220fa98d6af69f22c72fdb
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-09-19 22:29:35 +0700

    last-last style fixes

--- Task_1_1_2/src/test/java/ru/nsu/abramenko/MainTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/abramenko/MainTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
 import ru.nsu.abramenko.card.Card;
 import ru.nsu.abramenko.card.Deck;
 import ru.nsu.abramenko.card.Rank;

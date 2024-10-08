9c22dc491b79a79fe8bfea0d86095b084f0f6c10
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-10-08 21:27:36 +0700

    fix order

--- Task_1_1_3/src/test/java/ru/nsu/abramenko/math/DivTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/abramenko/math/DivTest.java
@@ -1,7 +1,7 @@
 package ru.nsu.abramenko.math;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;

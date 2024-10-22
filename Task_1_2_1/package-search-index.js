c242572181ce25cbbde4cc5727c8828a0bd1fe40
Vovadinamik8913 <Vovadinamik8913@users.noreply.github.com>
2024-10-22 06:34:37 +0000

    Deploying to gh-pages from @ Vovadinamik8913/OOP@3d0e67e4912ff7afd4cc94ab8acb3fe6cd855d00 🚀

--- Task_1_2_1/package-search-index.js
+++ Task_1_2_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.abramenko.graph"},{"l":"ru.nsu.abramenko.graph.basic"},{"l":"ru.nsu.abramenko.transform"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.abramenko.graph"},{"l":"ru.nsu.abramenko.graph.basic"},{"l":"ru.nsu.abramenko.graph.implement"}];updateSearchResults();

78e8d077f01f515ca3448d1d03b22a193c510070
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-11-19 15:38:13 +0700

    rem emoji

--- Task_1_3_1/src/test/java/ru/nsu/abramenko/SubstringTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/abramenko/SubstringTest.java
@@ -141,6 +141,7 @@ class SubstringTest {
         assertEquals(res.size(), 2500);
         new File(filePath).delete();
     }
+
     @Test
     @DisplayName("Chinese Test")
     public void chineseTest() throws IOException {
@@ -151,23 +152,6 @@ class SubstringTest {
         List<Long> test = new ArrayList<>();
         test.add(4L);
         List<Long> res = Substring.find(filePath, "是一个");
-        assertEquals(res, test);
-
-        new File(filePath).delete();
-    }
-
-    @Test
-    @DisplayName("Emoji Test")
-    public void emojiTest() throws IOException {
-        String filePath = "testFile.txt";
-        String content = "\uD83D\uDE02\uD83D\uDE18❤\uFE0F\uD83D\uDE0D\uD83D"
-                + "\uDC8B\uD83D\uDE04\uD83D\uDE07\uD83D\uDE19\uD83D\uDE1E"
-                + "\uD83E\uDD14\uD83E\uDD75\uD83E\uDD2F\uD83E\uDD2C";
-
-        createTestFile(filePath, content);
-        List<Long> test = new ArrayList<>();
-        test.add(8L);
-        List<Long> res = Substring.find(filePath, "\uD83D\uDC8B");
         assertEquals(res, test);
 
         new File(filePath).delete();
